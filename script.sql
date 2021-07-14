07daadecd1c02f5a1537cdbbaed37f2a8ac54336
Bruno Amorim <>
2021-07-14 17:41:09 -0300

    Administração e Gerenciamento de Banco de Dados: Criação documento de relatório

--- script.sql
+++ script.sql
@@ -1,9 +1,9 @@
 --****************************************************************************
 -- CRIANDO AS TABELAS
 --****************************************************************************
-DROP TABLE localizacao;
-DROP TABLE casos_covid;
-DROP TABLE relatorio;
+-- DROP TABLE localizacao;
+-- DROP TABLE casos_covid;
+-- DROP TABLE relatorio;
 
 CREATE TABLE IF NOT EXISTS casos_covid (
   idcasos_covid INT PRIMARY KEY,
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS casos_covid (
   last_available_deaths VARCHAR(45) NULL,
   new_confirmed INT NULL,
   new_deaths INT NULL
-)
+);
 
 CREATE TABLE IF NOT EXISTS localizacao (
   city VARCHAR(45) NULL,
@@ -31,29 +31,38 @@ CREATE TABLE IF NOT EXISTS localizacao (
   CONSTRAINT fk_localização_casos_covid
     FOREIGN KEY (idcasos_covid)
     REFERENCES casos_covid (idcasos_covid)
-)
+);
+
+delete from localizacao where idcasos_covid = 42;
+delete from casos_covid where idcasos_covid = 42;
+
+select * From localizacao where idcasos_covid = 42
+select * From casos_covid where idcasos_covid = 42
+
 
 create table relatorio
 as 
 SELECT city, sum(new_confirmed) as new_confirmed FROM casos_covid NATURAL JOIN localizacao
-GROUP BY city
+GROUP BY city;
 
+select * from relatorio
 --****************************************************************************
 -- CRIANDO INDICES
 --****************************************************************************
 
+select count(*) from localizacao
 SELECT * FROM casos_covid LIMIT 10;
 SELECT * FROM localizacao LIMIT 10;
 
 -- aqui usaremos o gin, um tipo de indice especilizada em buscas textuais de Full Text Search
 explain analyze
-select * from localizacao where city LIKE 'mS%';
+select * from localizacao where city LIKE 'Rio%';
 
 -- CREATE EXTENSION pg_trgm;
 create index idx_city on localizacao using GIN(city gin_trgm_ops);
 
 explain analyze
-select * from localizacao where city LIKE 'São%';
+select * from localizacao where city LIKE 'Rio%';
 
 -- Criaremos um indice de bitmap para a consultas de baixa seletividade como o campo Estado. Nesse caso o indice bipmap é recomendado atributos discretos com até 7 tipos.
 
@@ -110,8 +119,6 @@ select * from localizacao where city_ibge_code LIKE '33%';
 -- CRIANDO TRIGGER
 --****************************************************************************
 
-select * from relatorio
-
 -- DROP TRIGGER trg_atualiza_relatorio on casos_covid;
 -- DROP FUNCTION atualiza_relatorio;
 
@@ -135,8 +142,6 @@ CREATE TRIGGER trg_atualiza_relatorio
 AFTER INSERT OR DELETE OR UPDATE ON casos_covid FOR EACH ROW 
 EXECUTE PROCEDURE atualiza_relatorio(); 
 
--- update relatorio set new_confirmed = 0
-
 insert into casos_covid 
 (
 	idcasos_covid,
@@ -153,7 +158,7 @@ insert into casos_covid
 	new_deaths
 )
 values (
-101,
+42,
 2021-07-14,
 10,
 2021-07-14,
@@ -170,15 +175,28 @@ values (
 -- CRIANDO USUÁRIOS
 --****************************************************************************
 
--- Simularemos a criação de um cenários com dois perfis:
+-- Simularemos a criação de um cenários com três perfis:
 -- gestores: são responsaveis por gerenciar a inclusão e atualização de casos de covid.
 -- analistas: podem somente ter permissão de leitura, não podendo fazer qualquer tipo de atualização ou inserção de novos registros.
+-- administradores: 
 
+CREATE ROLE administradores LOGIN;
 CREATE ROLE gestores LOGIN;
 CREATE ROLE analistas LOGIN;
 
 GRANT SELECT ON casos_covid TO analistas;
-GRANT UPDATE, INSERT ON casos_covid TO gestores;
+GRANT SELECT, UPDATE, INSERT ON casos_covid TO gestores;
+GRANT ALL PRIVILEGES ON casos_covid TO administradores;
+
+CREATE USER user_adm WITH PASSWORD 'minhasenha';
+CREATE USER user_gestor WITH PASSWORD 'minhasenha';
+CREATE USER user_analista WITH PASSWORD 'minhasenha';
+
+GRANT administradores TO user_adm;
+GRANT gestores TO user_gestor;
+GRANT analistas TO user_analista;
+
+
 
 --****************************************************************************
 -- CRIANDO TRANSAÇÕES
